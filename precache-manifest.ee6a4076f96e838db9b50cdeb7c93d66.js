5acbf510d0ffb0222c9f5d2c3a58436fe71993c3
rushi.jagani <rushi.jagani@gmail.com>
2020-05-19 19:56:29 +0530

    Updates

---
+++ precache-manifest.ee6a4076f96e838db9b50cdeb7c93d66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f972fedb72259c4d1236e7140de0f6",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "908d3d547e1de4227752",
+    "url": "/react-todo/static/css/main.5be19d32.chunk.css"
+  },
+  {
+    "revision": "01010d988c5626a47b75",
+    "url": "/react-todo/static/js/2.4efa84a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.4efa84a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "908d3d547e1de4227752",
+    "url": "/react-todo/static/js/main.1355bf7e.chunk.js"
+  },
+  {
+    "revision": "68172ead1f0e2993076f",
+    "url": "/react-todo/static/js/runtime-main.a53a9e8b.js"
+  }
+]);
